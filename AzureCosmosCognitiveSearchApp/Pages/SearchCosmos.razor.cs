98d154cb9eb0594cbe9815215a53b044f2603272
Charlie Nathan <charlielewisnathan@gmail.com>
2022-12-27 14:28:13 +0000

    CS file for code behind new page

---
+++ AzureCosmosCognitiveSearchApp/Pages/SearchCosmos.razor.cs
@@ -0,0 +1,19 @@
+﻿using AzureCosmosCognitiveSearchApp.CosmosAPI;
+using AzureCosmosCognitiveSearchApp.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AzureCosmosCognitiveSearchApp.Pages
+{
+    public partial class SearchCosmos
+    {
+        [Inject]
+        CosmosDBService CosmosDBService { get; set; }
+
+        public List<Customer>? users { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
